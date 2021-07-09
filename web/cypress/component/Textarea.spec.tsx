7820643d410110d380508b7f38d1eb3a4f2404b5
Gustavo <gmcotta34@gmail.com>
2021-07-09 14:14:41 -0300

    test: add tests for <Textarea />

---
+++ web/cypress/component/Textarea.spec.tsx
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { mount } from "@cypress/react";
+
+import Textarea from '../../src/components/Textarea';
+import '../../src/assets/styles/global.css';
+import '../../src/components/Textarea/styles.css';
+
+context('<Textarea />', () => {
+  it('should render successfully', () => {
+    const label = 'Description';
+    const name = 'description';
+    const textareaValue = 'Some description';
+
+    mount(
+      <BrowserRouter>
+        <Textarea label={label} name={name} />
+      </BrowserRouter>
+    );
+
+    cy.get('label').as('label');
+    cy.get('textarea').as('textarea');
+
+    cy.get('@label').should('have.text', label);
+    cy.get('@label').invoke('attr', 'for').should('eq', name);
+
+    cy.get('@textarea').should('have.id', name);
+    cy.get('@textarea').type(textareaValue).should('have.value', textareaValue);
+  });
+});
+
