d8a9de841fbe5211d5535ae835640313984b0919
Gustavo <gmcotta34@gmail.com>
2021-07-09 14:03:07 -0300

    test: add tests for <Input />

---
+++ web/cypress/component/Input.spec.tsx
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { mount } from "@cypress/react";
+
+import Input from '../../src/components/Input';
+import '../../src/assets/styles/global.css';
+import '../../src/components/Input/styles.css';
+
+context('<Input />', () => {
+  before(() => {
+    cy.viewport(1366,768);
+  });
+
+  it('should render successfully', () => {
+    const label = 'Name';
+    const name = 'name';
+    const givenName = 'Gustavo';
+    
+    mount(
+      <BrowserRouter>
+        <Input label={label} name={name} />
+      </BrowserRouter>
+    );
+
+    cy.get('label').as('label');
+    cy.get('input').as('input');
+
+    cy.get('@label').should('have.text', label);
+    cy.get('@label').invoke('attr', 'for').should('eq', name);
+
+    cy.get('@input').should('have.id', name);
+    cy.get('@input').type(givenName).should('have.value', givenName);
+  });
+});
